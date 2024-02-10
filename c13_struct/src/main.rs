ff42f26ae6c8c4d402d6f42fd8acb89e827dc20d
洮羱芝闇 <shun_@outlook.com>
2024-02-10 23:43:06 +0800

    Update main.rs

--- c13_struct/src/main.rs
+++ c13_struct/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    // 定义结构体
+    // 元组结构体
+    struct Color(i32, i32, i32);
+    // 经典结构体
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+    // 单元结构体
+    #[derive(Debug)]
+    struct Unit;
+
+    // 初始化结构体
+    let mut black = Color(0, 0, 0);
+    let origin = Point { x: 0, y: 0 };
+    let unit = Unit;
+
+    // 访问结构体的字段
+    println!("black:{:?}", black.0);
+    println!("origin:{:?}", origin.x);
+    println!("unit:{:?}", unit);
+
+    // 修改结构体内容
+    black.0 = 255;
 }
