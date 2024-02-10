0c5d5b5afbcb89073b362ea31ad417fa38f64afb
洮羱芝闇 <shun_@outlook.com>
2024-02-10 23:09:33 +0800

    更新切片

--- c12_slice/src/main.rs
+++ c12_slice/src/main.rs
@@ -1,9 +1,24 @@
 fn main() {
+    // 切片的大小是可以在运行过程中改变的
     let mut v = Vec::new();
-    v.push("Go语言极简一本通");
-    v.push("Go语言微服务架构核心22讲");
-    v.push("从0到Go语言微服务架构师");
+    v.push("apple");
+    v.push("banana");
+    v.push("banana1");
     println!("len:{:?}",v.len());
     let s1=&v[1..3];
     println!("s1:{:?}",s1);
+    first_word(s1);
+    first_word1(&mut v);
+    println!("v:{:?}",v[0]);
+}
+
+// 切片可以用来当作参数
+fn first_word(s:&[&str]){
+    println!("s:{:?}",s[1]);
+}
+
+// 可变切片
+fn first_word1(s:&mut[&str]){
+    s[0]="apple96";
+    println!("s:{:?}",s[0]);
 }
