cfc7ec3f3fffdccdbe9344bcd81807721c19ccda
洮羱芝闇 <shun_@outlook.com>
2024-02-11 15:55:41 +0800

    更新async-std内容

--- c20_closure/src/main.rs
+++ c20_closure/src/main.rs
@@ -23,4 +23,58 @@ fn main() {
     let x = 1;
     let add_x = |y| x + y; // x是一个周围作用域中的变量 y是传入的
     println!("{:?}", add_x(1));
+
+
+    let add = |x, y| x + y;
+    receive_closure(add);
+
+    let z = 3; //闭包函数仍然可以捕获到变量
+    receive_closure1(|x| x + z);
+
+    // 闭包作为参数和返回值
+    let closure  = return_closure();
+    println!("{}", closure(1));
+
+    let closure1 = do1(add,1);
+    println!("{}", closure1(2));
+
+    let closure2 = do2(add,1);
+    println!("{:?}", closure2(2));
+}
+
+// 函数接收一个闭包
+fn receive_closure<F>(f: F)
+    where F: Fn(i32, i32) -> i32
+{
+    let result = f(1, 2);
+    println!("xxx{}", result);
+}
+
+
+fn receive_closure1<F>(f: F)
+    where F: Fn(i32) -> i32
+{
+    let result = f(1);
+    println!("yyy{}", result);
+}
+
+// 函数返回一个闭包
+fn return_closure() -> impl Fn(i32) -> i32 {
+    |x| x + 1
+}
+
+// 参数和返回值都有闭包
+fn do1<F>(f: F,x:i32) -> impl Fn(i32) -> i32
+    where F: Fn(i32,i32) -> i32
+{
+    // 传进来的闭包f和参数x ，y需要调用返回的闭包的时候传入
+    move |y| f(x,y)
+}
+
+// 通用形式的闭包方法
+fn do2<F,X,Y,Z>(f:F,x:X) -> impl Fn(Y) -> Z
+    where F: Fn(X,Y) -> Z,
+    X: Copy
+{
+    move |y| f(x,y)
 }
--- c29_async-await/src/main.rs
+++ c29_async-await/src/main.rs
@@ -1,3 +1,75 @@
-fn main() {
-    println!("Hello, world!");
+use std::future::Future;
+use async_std::task::{block_on, sleep, spawn};
+
+#[async_std::main]
+async fn main() {
+    // // do3();
+    // // do4();
+    // let do3_span = spawn(do3);
+    // let do4_span = spawn(do4);
+    //
+    // // 主线程必须等do3和do4执行完
+    // do3_span.join().unwrap();
+    // do4_span.join().unwrap();
+
+    // let do5_spawn = spawn(do5());
+    // // 直接调用do6() 会报错 因为do6()是一个异步函数 必须在async函数中调用 后面街上await
+    // do6().await;
+    // // 主线程必须等do5和do6执行完
+    // do5_spawn.await;
+
+    let result = block_on(test2());
+    println!("{}", result);
 }
+
+// async 异步函数
+async fn test() ->String{
+    String::from("test")
+}
+
+fn test1() ->impl Future<Output = String>{
+    async{
+        let x = test().await;
+        x + "9"
+    }
+}
+
+fn test2() ->impl Future<Output = String>{
+    let r = |x:String| async move{
+        // y是test9
+        let  y = test1().await;
+        // 它是这个闭包函数的返回值
+        y + &*x
+    };
+    // 传入x为6
+    r(String::from("6"))
+}
+
+// fn do3(){
+//     for i in 1..10 {
+//         println!("{}", i);
+//         sleep(Duration::from_millis(500));
+//     }
+// }
+//
+// fn do4(){
+//     for i in 1..10 {
+//         println!("{}", i);
+//         sleep(Duration::from_millis(1000));
+//     }
+// }
+
+
+// async fn do5(){
+//     for i in 1..10 {
+//         println!("do5 {}", i);
+//         sleep(Duration::from_millis(500)).await;
+//     }
+// }
+//
+// async fn do6(){
+//     for i in 1..10 {
+//         println!("do6 {}", i);
+//         sleep(Duration::from_millis(1000)).await;
+//     }
+// }
