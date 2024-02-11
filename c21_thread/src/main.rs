9b236d099d821e4a38bc9574f6f05668098b53c6
洮羱芝闇 <shun_@outlook.com>
2024-02-11 09:41:26 +0800

    更新到线程

--- c21_thread/src/main.rs
+++ c21_thread/src/main.rs
@@ -1,3 +1,21 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    //子线程
+    let handler = thread::spawn(|| {
+        for i in 1..10 {
+            println!("子线程 {}", i);
+            // sleep 会阻塞当前线程 某个线程睡眠的时候会让出CPU
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    // 主线程 当主线程结束时，子线程也会结束
+    for i in 1..5 {
+        println!("主线程 {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // 等handler对应待子线程结束才继续 否则在这阻塞
+    handler.join().unwrap();
 }
